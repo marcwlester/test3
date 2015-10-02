8cde1b24857ef2552eda208e76e340cbcff1dcd2
Marc Lester <marc.w.lester@gmail.com>
2015-10-01 23:20:41 -0400

    not sure why, but functions in other files aren't being loaded?  my player functions were not visible to the bootstrap.js file so i put them in there for now

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -25,3 +25,25 @@ Meteor.users.find({ "status.online": true }).observe({
     }
   }
 });
+
+function findPlayer(id)
+{
+  return Players.findOne({userId: id});
+}
+
+function createPlayer(user, playerData)
+{
+  Players.insert({
+    userId: user._id,
+    username: user.username,
+    playerData: playerData,
+    active: true
+  });
+}
+
+function setPlayerActive(player, active)
+{
+    Players.update(player._id, {
+      $set: {active: active}
+    });
+}
